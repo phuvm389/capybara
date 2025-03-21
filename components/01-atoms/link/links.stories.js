9a6624ded5fc52a8df6b8e79b4031cbda6023ba2
Phu Vo <phuvm389@gmail.com>
2025-03-21 09:32:48 +0700

    add hero varidants

--- components/01-atoms/link/links.stories.js
+++ components/01-atoms/link/links.stories.js
@@ -4,15 +4,15 @@ import linkData from './link.json';
 /**
  * Storybook Definition.
  */
-export default { title: 'Atoms/Button' };
+export default { title: 'Atoms/Link' };
 
 // Default button story.
 export const link = {
   render: (args) => linkTwig(args),
   args: { ...linkData },
   argTypes: {
-    class: {
-      options: ['primary', 'secondary'],
+    variants: {
+      options: ['default', 'btn btn--primary', 'btn btn--secondary'],
       control: { type: 'select' },
     },
   },
--- components/02-molecules/hero/hero.stories.js
+++ components/02-molecules/hero/hero.stories.js
@@ -1,5 +1,6 @@
 import heroTwig from './hero.twig';
 import heroData from './hero.json';
+import heroLargeData from './hero--large.json';
 import './hero.scss';
 
 /**
@@ -7,7 +8,25 @@ import './hero.scss';
  */
 export default { title: 'Molecules/Hero' };
 
+const argTypes = {
+  variants: {
+    options: ['default', 'hero--large'],
+    control: { type: 'select' },
+  },
+  section_space: {
+    options: ['section-space', 'no-section-space'],
+    control: { type: 'select' },
+  },
+};
+
 export const hero = {
   render: (args) => heroTwig(args),
   args: { ...heroData },
+  argTypes,
+};
+
+export const heroLarge = {
+  render: (args) => heroTwig(args),
+  args: { ...heroLargeData },
+  argTypes,
 };
